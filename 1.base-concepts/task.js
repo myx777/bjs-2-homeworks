12768781b2111bfccefb0c476d00ab7114b3fc3d
MYX <it.anton22@gmail.com>
2023-05-14 21:14:20 +0300

    исправил замечания, добавил проверку на 'число'

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,15 +2,15 @@
 
 
 function solveEquation(a, b, c) {
-  let arr = [];
-  const d = b**2 - 4 * a * c; 
-  if (d === 0) {
+  const arr = [];
+  const discriminat = b**2 - 4 * a * c; 
+  if (discriminat === 0) {
     let x = -b / ( 2 * a );
     arr.push(x);
     return arr;
-  } else if (d > 0) {
-    let x1 = (-b + Math.sqrt(d)) / (2 * a);
-    let x2 = (-b - Math.pow(d, 1 / 2) )/( 2 * a);
+  } else if (discriminat > 0) {
+    let x1 = (-b + Math.sqrt(discriminat)) / (2 * a);
+    let x2 = (-b - Math.pow(discriminat, 1 / 2) )/( 2 * a);
     arr.push(x1, x2)
     return arr;
   } else { 
@@ -21,10 +21,16 @@ function solveEquation(a, b, c) {
 
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  const percentInMouth = (percent / 100) / 12;
-  const bodyCredit = amount - contribution;
-  const payment = bodyCredit * (percentInMouth + (percentInMouth / 
-    ((Math.pow((1 + percentInMouth), countMonths)) - 1)));
-  const totalSumm = payment * countMonths;
-  return +totalSumm.toFixed(2);
+  if (Number.isFinite(+percent) && Number.isFinite(+contribution) && 
+      Number.isFinite(+amount) && Number.isFinite(+countMonths)) {
+    const percentInMouth = (+percent / 100) / 12;
+    const bodyCredit = +amount - +contribution;
+    const payment = bodyCredit * (percentInMouth + (percentInMouth / 
+      ((Math.pow((1 + percentInMouth), +countMonths)) - 1)));
+    const totalSumm = payment * +countMonths;
+    return +totalSumm.toFixed(2);
+  } else {
+    return false;
+  }
+
 };
