d173ec94fb7838b1bb97a8f52b2563f9e1dc4819
MYX <it.anton22@gmail.com>
2023-05-22 22:07:28 +0300

    Тема функции выполнинил задание 1 с помощью цикла

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,25 @@
 function getArrayParams(...arr) {
+  let max = -Infinity;
+  let min = Infinity;
+  let sum = 0;
+  let avg = 0;
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i] > max){
+      max = arr[i];
+    } 
+    
+    if (arr[i] < min){
+      min = arr[i];
+    }
 
+    sum += arr[i];
+  }
+  avg = sum / arr.length;
+  avg = +avg.toFixed(2);
   return { min: min, max: max, avg: avg };
 }
+
+
 
 function summElementsWorker(...arr) {
 
