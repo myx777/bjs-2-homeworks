508c4b1552b099b609b13c56a2aa1259c23eb611
MYX <it.anton22@gmail.com>
2023-05-24 12:03:16 +0300

    Функции. задание 2 выполнил полностью

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -11,23 +11,55 @@ function getArrayParams(...arr) {
   return { min: min, max: max, avg: avg };
 };
 
-
-
 function summElementsWorker(...arr) {
-
-}
+  const sum = arr.reduce((summ, item) => summ + item, 0);
+  return sum;
+};
 
 function differenceMaxMinWorker(...arr) {
-
-}
+  let differentNumber;
+  (arr.length > 0) ? differentNumber = Math.max(...arr) - Math.min(...arr) : differentNumber = 0;
+  return differentNumber;
+};
 
 function differenceEvenOddWorker(...arr) {
+  let sumEvenElement = 0;
+  let sumOddElement = 0;
+  if (arr.length != 0) {
+    for (let num of arr) {
+      if (num % 2 === 0) {
+        sumEvenElement += num;
+      } else if (num % 2 === 1) {
+        sumOddElement += num;
+      }
 
-}
+    };
+
+  } else {
+    return 0;
+  }
+
+  return sumEvenElement - sumOddElement;
+};
 
 function averageEvenElementsWorker(...arr) {
+  let sumEvenElement = 0;
+  let countEvenElement = 0;
+  if (arr.length === 0) {
+    return 0;
+  } else {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] % 2 === 0) {
+        sumEvenElement += arr[i];
+        countEvenElement += 1;
+      }
 
-}
+    };
+
+  }
+
+  return sumEvenElement / countEvenElement;
+};
 
 function makeWork (arrOfArr, func) {
 
