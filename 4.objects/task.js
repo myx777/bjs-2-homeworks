e526ef5ae848092d31d03f8b24cdec8f4f1e1dcd
MYX <it.anton22@gmail.com>
2023-06-14 20:21:58 +0300

    функции. выполнил полностью

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,29 @@
 function Student(name, gender, age) {
-  
-}
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
+};
+
+let student = new Student();
 
 Student.prototype.setSubject = function (subjectName) {
-  
-}
+    this.subject = subjectName;
+};
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
+Student.prototype.addMarks = function (...marksToAdd) {
+  if("marks" in this) {
+    this.marks.push(...marksToAdd);
+  }
+};
 
 Student.prototype.getAverage = function () {
-  
-}
+  return ("marks" in this && this.marks.length !== 0) ? 
+  this.marks.reduce((acc, item) => acc + item, 0) / this.marks.length : 0;
+};
 
 Student.prototype.exclude = function (reason) {
-  
-}
+  this.excluded = reason;
+  delete this.subject;
+  delete this.marks; 
+};
